6edc970e227795c11e0c727926e38b13afd589aa
Erwin Schwartz <erwin.schwartz.etudiant@gmail.com>
2016-04-08 12:05:14 +0200

    comexo_07

--- exo-7/hello/www/js/index.js
+++ exo-7/hello/www/js/index.js
@@ -16,6 +16,23 @@ var app = {
    // function, we must explicitly call 'app.receivedEvent(...);'
    onDeviceReady: function () {
       app.receivedEvent('deviceready');
+      app.checkConnection();
+
+
+   },
+
+   checkConnection: function () {
+      var networkState = navigator.connection.type;
+      var states = {};
+      states[Connection.UNKNOWN] = 'Unknown connection';
+      states[Connection.ETHERNET] = 'Ethernet connection';
+      states[Connection.WIFI] = 'WiFi connection';
+      states[Connection.CELL_2G] = 'Cell 2G connection';
+      states[Connection.CELL_3G] = 'Cell 3G connection';
+      states[Connection.CELL_4G] = 'Cell 4G connection';
+      states[Connection.CELL] = 'Cell generic connection';
+      states[Connection.NONE] = 'No network connection';
+      document.getElementById('network').innerHTML = 'Connection type : ' + states[networkState];
    },
    // Update DOM on a Received Event
    receivedEvent: function (id) {
