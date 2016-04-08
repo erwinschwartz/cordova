bf8c5c1a8d25f268806a8a5777e22786cdcfec5d
Erwin Schwartz <erwin.schwartz.etudiant@gmail.com>
2016-04-08 12:29:17 +0200

    comexo_10_nonfini

--- exo-10/hello/www/js/index.js
+++ exo-10/hello/www/js/index.js
@@ -16,6 +16,44 @@ var app = {
    // function, we must explicitly call 'app.receivedEvent(...);'
    onDeviceReady: function () {
       app.receivedEvent('deviceready');
+      app.contacts();
+   },
+
+   contacts: function () {
+      //console.log('test');
+      //alert('test');
+
+      $('#findButton').click(function () {
+         var finder = $('#find').val();
+         //document.getElementById('email').innerHTML = finder; // new Data();
+
+         function onSucess(contacts) {
+            $('#name').html(contacts[0].name.giveName + ' ' + contacts[0].name.familyName);
+            for (var i = 0; i < contacts[0].emails.length; i++) {
+               if (i > 0) {
+                  $('#email').append('</br>');
+               }
+               $('#email').append(contacts[0].emails[i].value);
+            }
+            for (var i = 0; i < contacts[0].phoneNumbers.length; i++) {
+               if (i > 0) {
+                  $('#tel').append('</br>');
+               }
+               $('#tel').append(contacts[0].phoneNumbers[i].value);
+            }
+         };
+
+         function onError(containerError) {
+            alert('onError!');
+         };
+
+         //find all contacts
+         var options = new ContactFindOption();
+         options.filter = finder;
+         var filteredFields = ["displayName", "name"];
+         //non fini
+
+      })
    },
    // Update DOM on a Received Event
    receivedEvent: function (id) {
